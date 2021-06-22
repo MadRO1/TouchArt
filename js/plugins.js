c1f548309c6950a3d9bea2e4cc8cf405535d1ade
madyan <83299777+MadRO1@users.noreply.github.com>
2021-06-22 09:08:26 +0200

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -1,15 +1,4 @@
 $(function() {
-    $(window).scroll(function(){
-        var scrolling = $(this).scrollTop();
-        var divoffset = $("#our-plans").offset().top();
-        if(scrolling >= divoffset){
-            $("#scrollingnav").fadeIn();
-        }else{
-            $("#scrollingnav").fadeOut();
-        }
-    });
-
-
     $(".content").hide();
     $(".show_hide").on("click", function () {
         var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
@@ -19,10 +8,6 @@ $(function() {
     $('.carousel').carousel({
         interval: 1000 * 10
     });
-   
-}); 
-$(function(){
-
    
 });
 
